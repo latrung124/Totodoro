37b8f6add0da3bded40808fd9946c1464171d1d4
trung.la <laquangtrung1241997nd@gmail.com>
2025-02-05 15:08:39 +0700

    Add pre-commit

--- main.cpp
+++ main.cpp
@@ -6,36 +6,36 @@
 void setApplicationAttribute()
 {
 #if defined(Q_OS_MACOS)
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 }
 
 void setApplicationInfo()
 {
-    using namespace config::const_val::application;
+	using namespace config::const_val::application;
 
-    QGuiApplication::setOrganizationDomain(OrganizationDomain);
-    QGuiApplication::setOrganizationName(OrganizationName);
-    QGuiApplication::setApplicationName(ApplicationName);
-    QGuiApplication::setApplicationDisplayName(ApplicationDisplayName);
-    QGuiApplication::setApplicationVersion(ApplicationVersion);
+	QGuiApplication::setOrganizationDomain(OrganizationDomain);
+	QGuiApplication::setOrganizationName(OrganizationName);
+	QGuiApplication::setApplicationName(ApplicationName);
+	QGuiApplication::setApplicationDisplayName(ApplicationDisplayName);
+	QGuiApplication::setApplicationVersion(ApplicationVersion);
 }
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+	QGuiApplication app(argc, argv);
 
-    setApplicationAttribute();
-    setApplicationInfo();
+	setApplicationAttribute();
+	setApplicationInfo();
 
-    QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("Totodoro", "Main");
+	QQmlApplicationEngine engine;
+	QObject::connect(
+	    &engine,
+	    &QQmlApplicationEngine::objectCreationFailed,
+	    &app,
+	    []() { QCoreApplication::exit(-1); },
+	    Qt::QueuedConnection);
+	engine.loadFromModule("Totodoro", "Main");
 
-    return app.exec();
+	return app.exec();
 }
