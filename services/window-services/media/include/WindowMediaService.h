c31651130f7e4504e87d65c4b01c452e5023cf27
trung.la <laquangtrung1241997nd@gmail.com>
2025-02-08 21:27:56 +0700

    Implement window media service

---
+++ services/window-services/media/include/WindowMediaService.h
@@ -0,0 +1,18 @@
+/*
+* File: WindowMediaService.h
+* Author: trung.la
+* Date: 02-07-2025
+* Description: This file contains the declaration of the WindowMediaService class
+*/
+
+#ifndef WINDOW_MEDIA_SERVICE_H
+#define WINDOW_MEDIA_SERVICE_H
+
+class WindowMediaService
+{
+public:
+    WindowMediaService();
+    ~WindowMediaService();
+};
+
+#endif // WINDOW_MEDIA_SERVICE_H
---
+++ services/window-services/media/src/WindowMediaService.cpp
@@ -0,0 +1,16 @@
+/*
+* File: WindowMediaService.cpp
+* Author: trung.la
+* Date: 02-07-2025
+* Description: This file contains the implementation of the WindowMediaService class
+*/
+
+#include "WindowMediaService.h"
+
+WindowMediaService::WindowMediaService()
+{
+}
+
+WindowMediaService::~WindowMediaService()
+{
+}
