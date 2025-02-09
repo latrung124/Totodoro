2b3ebd5c460eeb1ad025ee4c7a83bda434d24c0a
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-02-09 22:30:35 +0700

    Implement WMediaServiceListener class

---
+++ core/handlers/service-listeners/WMediaServiceListener.cpp
@@ -0,0 +1,20 @@
+/*
+* File: WMediaServiceListener.cpp
+* Author: trung.la
+* Date: 02-09-2025
+* Description: Interface for media service listener
+*/
+
+#include "WMediaServiceListener.h"
+
+void WMediaServiceListener::onMediaInfoChanged(const WMediaInfo &mediaInfo)
+{
+}
+
+void WMediaServiceListener::onPlaybackControlsChanged(const WPlaybackControls &playbackControls)
+{
+}
+
+void WMediaServiceListener::onPlaybackStatusChanged(const WPlaybackInfo &playbackInfo)
+{
+}
---
+++ core/handlers/service-listeners/WMediaServiceListener.h
@@ -0,0 +1,24 @@
+/*
+* File: WMediaServiceListener.h
+* Author: trung.la
+* Date: 02-09-2025
+* Description: Interface for media service listener
+*/
+
+#ifndef W_MEDIA_SERVICE_LISTENER_H
+#define W_MEDIA_SERVICE_LISTENER_H
+
+#include "interfaces/window-services/IWMediaServiceListener.h"
+
+class WMediaServiceListener : public IWMediaServiceListener
+{
+public:
+    WMediaServiceListener() = default;
+    virtual ~WMediaServiceListener() = default;
+
+    void onMediaInfoChanged(const WMediaInfo &mediaInfo) override;
+    void onPlaybackControlsChanged(const WPlaybackControls &playbackControls) override;
+    void onPlaybackStatusChanged(const WPlaybackInfo &playbackInfo) override;
+};
+
+#endif // W_MEDIA_SERVICE_LISTENER_H
