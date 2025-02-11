0fb208033127289b3471b730b5d3df82761945d4
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-02-11 23:33:36 +0700

    Implement Service Manager class

---
+++ core/services/ServiceManager.cpp
@@ -0,0 +1,19 @@
+/*
+* File: ServiceManager.cpp
+* Author: trung.la
+* Date: 02-11-2025
+* Description: This file contains the implementation of the ServiceManager class.
+*/
+
+#include "core/services/ServiceManager.h"
+
+ServiceManager& ServiceManager::instance()
+{
+    static ServiceManager serviceManager;
+    return serviceManager;
+}
+
+ServiceManager::~ServiceManager()
+{
+    m_services.clear();
+}
