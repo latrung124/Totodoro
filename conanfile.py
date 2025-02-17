6225e338b01fde2ae037b596cbe91ec4bfa011be
latrung124 <53443778+latrung124@users.noreply.github.com>
2025-02-17 23:26:52 +0700

    Fix conan install failed by missing setting

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,8 @@
 from conan import ConanFile
 
 class ConanPackage(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
     def requirements(self):
         self.requires("gtest/1.15.0")
 
