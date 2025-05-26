39caed54980335f9e5173d0aa3d9affc1b2a2eca
trung.la <laquangtrung1241997nd@gmail.com>
2025-05-26 22:26:44 +0700

    Implement microservice skeleton architecture

---
+++ microservices/service-discovery/ConsulDiscovery.h
@@ -0,0 +1,40 @@
+/*
+* File: ConsulDiscovery.h
+* Author: trung.la
+* Date: 03-26-2025
+* Description: Implementation of service discovery using Consul
+* Consul is a distributed service mesh to connect, secure, and configure services across any runtime platform and public or private cloud
+*/
+
+#ifndef CONSULDISCOVERY_H_
+#define CONSULDISCOVERY_H_
+
+#include <string_view>
+
+#include <httplib.h>
+
+#include "microservices/common/service-discovery/IServiceDiscovery.h"
+
+namespace microservices::service_discovery
+{
+
+class ConsulDiscovery : public IServiceDiscovery
+{
+public:
+    ConsulDiscovery(std::string_view addr = "localhost", int port = 8500, bool enabledHealthCheck = true);
+    virtual ~ConsulDiscovery();
+
+    virtual std::vector<std::string> discover(const std::string& name) override;
+    virtual void registerService(const std::string &name, const std::string &url, int port) override;
+    virtual void unregisterService(const std::string &name, const std::string &url, int port) override;
+
+private:
+    std::string getLocalIp();
+
+    httplib::Client m_client;
+    bool m_enabledHealthCheck = true;
+};
+
+} // namespace microservices::service_discovery
+
+#endif // CONSULDISCOVERY_H_
