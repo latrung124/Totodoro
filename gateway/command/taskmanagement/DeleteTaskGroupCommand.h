01379a7f60863ca88d863f1b13461d2f88d761d9
trung.la <laquangtrung1241997nd@gmail.com>
2025-09-11 22:18:50 +0700

    Implement DeleteTaskGroupCommand

---
+++ gateway/command/taskmanagement/DeleteTaskGroupCommand.h
@@ -0,0 +1,48 @@
+/**
+ * @file DeleteTaskGroupCommand.h
+ * @author trung.la
+ * @date 09-11-2025
+ * @brief Header file for DeleteTaskGroupCommand class that handles deleting a task group via the OpenAPI client.
+ */
+
+#pragma once
+
+#include <QNetworkReply>
+
+#include "ApiClientFactory.h"
+#include "IApiCommand.h"
+
+#include <OAITaskManagementServiceApi.h>
+#include <OAITask_managementDeleteTaskGroupResponse.h>
+
+class DeleteTaskGroupCommand : public IApiCommand
+{
+    Q_OBJECT
+public:
+    using OAITask_managementDeleteTaskGroupResponse = OpenAPI::OAITask_managementDeleteTaskGroupResponse;
+    DeleteTaskGroupCommand(const QString& groupId,
+        ApiClientFactoryPtr factory,
+        const QString& baseUrl,
+        QObject* parent = nullptr);
+    ~DeleteTaskGroupCommand() override = default;
+
+    void execute() override;
+    void setResponseHandler(IResponseHandlerPtr handler) override;
+    IResponseHandlerPtr getResponseHandler() const override;
+
+    bool isDeleted() const;
+
+private slots:
+    // Private slots to handle API responses
+    void onTaskGroupDeleted(const OAITask_managementDeleteTaskGroupResponse& response);
+    void onTaskGroupError(const OAITask_managementDeleteTaskGroupResponse& response,
+                       QNetworkReply::NetworkError errorType, const QString& errorStr);
+
+private:
+    bool mDeleted;
+    QString mGroupId;
+    ApiClientFactoryPtr mApiClientFactory;
+    QString mBaseUrl;
+    IResponseHandlerPtr mResponseHandler;
+    std::unique_ptr<OpenAPI::OAITaskManagementServiceApi> mApiClient;
+};
